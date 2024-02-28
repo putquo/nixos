1f4504ebff2d89415c464ddb75577245ec07913d
Jan-Justin van Tonder <jan.justin.vtonder@gmail.com>
2024-02-28 13:37:10 +0100

    feat(formelio): enable k9s

--- presets/user/formelio.nix
+++ presets/user/formelio.nix
@@ -41,6 +41,8 @@
         programs = {
           git.extraConfig.user.signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBRTP702lUz73eZnq5TZXdkb2AkNvJbNuHLBXt42kv66";
           git.userEmail = "jan.justin.vtonder@gmail.com";
+
+          k9s.enable = true;
         };
 
         xdg.desktopEntries.steam.exec = "";
