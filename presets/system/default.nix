0c47a953ed46de5b61483ec46ab577ea3a19f44f
Preston van Tonder <46090392+putquo@users.noreply.github.com>
2024-04-14 11:36:15 +0200

    refactor: convert base presets to default presets

--- presets/system/default.nix
+++ presets/system/default.nix
@@ -1,6 +1,7 @@
-{
+{ config, lib, pkgs, ... }: with lib; let
+  wsl = config.wsl.enable;
+in {
   imports = [
-    ./base.nix
     ./gaming.nix
     ./gnome.nix
     ./i18n
@@ -8,4 +9,87 @@
     ./nvidia.nix
     ./security.nix
   ];
+
+  options = {
+    presets.system.default.enable = mkOption {
+      type = types.bool;
+      description = "Whether to enable the base system preset.";
+      default = true;
+      example = true;
+    };
+  };
+
+  config = mkIf config.presets.system.default.enable {
+    environment.systemPackages = with pkgs; [
+      curl
+      fzf
+      git
+      go
+      jq
+      libgccjit
+      lua
+      nodejs_latest
+      protonvpn-gui
+      ripgrep
+      tree
+      unzip
+      vim
+      wireguard-tools
+      wget
+      which
+      zip
+    ];
+
+    fonts.packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "CascadiaCode" "JetBrainsMono" ]; })
+    ];
+
+    home-manager.useGlobalPkgs = true;
+    home-manager.useUserPackages = true;
+
+    programs._1password-gui.enable = mkIf (!wsl) true;
+
+    nix.extraOptions = "warn-dirty = false";
+    nix.gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 1w";
+    };
+    nix.settings = {
+      auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+    };
+
+    nixpkgs.config.allowUnfree = true;
+
+    services.tailscale.enable = true;
+
+    services.xserver.desktopManager.xterm.enable = false;
+    services.xserver.excludePackages = [ pkgs.xterm ];
+
+    systemd.services.docker-desktop-proxy.script = mkForce ''${config.wsl.wslConf.automount.root}/wsl/docker-desktop/docker-desktop-user-distro proxy --docker-desktop-root ${config.wsl.wslConf.automount.root}/wsl/docker-desktop "C:\Program Files\Docker\Docker\resources"'';
+
+    virtualisation = {
+      docker = {
+        enable = true;
+        enableOnBoot = true;
+        autoPrune.enable = true;
+      };
+
+      podman = mkIf (!wsl) {
+        enable = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+    };
+
+    wsl.extraBin = with pkgs; [
+      { src = "${coreutils}/bin/mkdir"; }
+      { src = "${coreutils}/bin/cat"; }
+      { src = "${coreutils}/bin/whoami"; }
+      { src = "${coreutils}/bin/ls"; }
+      { src = "${busybox}/bin/addgroup"; }
+      { src = "${su}/bin/groupadd"; }
+      { src = "${su}/bin/usermod"; }
+    ];
+  };
 }
