putquo/nixconf

refactor: repurpose with humea

nix/nixos/system/byte.nix
@@ -2,14 +2,13 @@
   host = cell.host.${name};
 
   profiles = [
-    super.profile.cosmic
     super.profile.development
+    super.profile.gnome
     super.profile.laptop
-    super.profile.wayland
-    super.profile.wazuh
+    # super.profile.wazuh
   ];
 
   users = [
-    cell.user.uniform
+    cell.user.toil
   ];
 }

putquo/nixos

nix/nixos/system/byte.nix
@@ -3,13 +3,12 @@
 
   profiles = [
     super.profile.cosmic
+    super.profile.desktop
     super.profile.development
-    super.profile.laptop
-    super.profile.wayland
-    super.profile.wazuh
+    # super.profile.wazuh
   ];
 
   users = [
-    cell.user.uniform
+    cell.user.toil
   ];
 }