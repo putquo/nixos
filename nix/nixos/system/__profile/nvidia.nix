5e9b910cb7b5522dd9e7dea293a673100b9ddfe1
Preston van Tonder <46090392+putquo@users.noreply.github.com>
2025-02-01 15:28:13 +0100

    fix: use open source nvidia kernel module

--- nix/nixos/system/__profile/nvidia.nix
+++ nix/nixos/system/__profile/nvidia.nix
@@ -1,10 +1,8 @@
 { ... }@_haumeaArgs:
 { config, ... }@_nixosModuleArgs: {
-  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
-
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.nvidiaSettings = true;
-  hardware.nvidia.open = false;
+  hardware.nvidia.open = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
   hardware.nvidia.powerManagement.enable = true;
 
